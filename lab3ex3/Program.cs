38fccdb3e7bc87b22dc0930762d6875ba147bdcc
teoi89 <teofil.lucaci@yahoo.ro>
2022-12-13 17:28:46 +0200

    Add project files.

---
+++ lab3ex3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab3ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti o functie care va determina daca un numar este sau nu
+             * patrat perfect. Apelati-o si afisati-i rezultatul */
+            Console.WriteLine("Add a number");
+            
+            int number = int.Parse(Console.ReadLine());
+            
+            if (GetSquareNumber(number))
+            {
+                Console.WriteLine("The number introduced is a perfect sqare");
+            }
+            else
+            {
+                Console.WriteLine("The number introduced is not a perfect sqare");
+            }
+        }
+        public static bool GetSquareNumber(int number)
+        {
+            bool results = false;
+            double sqare = (int)Math.Sqrt(number);
+            results = number == sqare * sqare;
+            return results;
+           
+        }
+    }
+}
